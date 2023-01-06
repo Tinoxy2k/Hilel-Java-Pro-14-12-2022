842fd11c16c4b15e4bd6d5cb981c3299cb7771bd
Tinoxy2k <51656455+Tinoxy2k@users.noreply.github.com>
2023-01-06 11:17:34 +0100

    add class "main"

---
+++ src/main/Java/Animals/Main.java
@@ -0,0 +1,20 @@
+package Animals;
+
+public class Main {
+    public static void main(String[] args) {
+    Cat cat = new Cat("Mellisa", 200,5);
+    Dog dog = new Dog("Ray", 500, 10);
+        cat.swim();
+        cat.run();
+        dog.run();
+        dog.swim();
+        Animal.getNumberOfObjects();
+
+
+
+
+
+
+
+    }
+}
