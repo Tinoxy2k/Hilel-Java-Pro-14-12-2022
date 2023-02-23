56682e0be2a0e0226bd2b96a1efc0db49d70db2d
Tinoxy <Tinoxy2k@gmail.com>
2023-02-23 18:29:02 +0100

    Add Methods

--- src/main/Homework7/BookSerialize.java
+++ src/main/Homework7/BookSerialize.java
@@ -1,4 +1,14 @@
 package main.Homework7;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class BookSerialize {
+    public void serialize(BookStatistic book, File file) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        writer.write(book.toString());
+        writer.close();
+    }
 }
