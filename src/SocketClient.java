aed5c458b0bbcdbe9b01be8313e682232803e56c
Tinoxy <Tinoxy2k@gmail.com>
2023-02-28 16:15:31 +0100

    implement variables and methods to SocketClient.java

--- src/SocketClient.java
+++ src/SocketClient.java
@@ -1,2 +1,24 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class SocketClient {
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost", 8081);
+             DataOutputStream request = new DataOutputStream(socket.getOutputStream());
+             DataInputStream answer = new DataInputStream(socket.getInputStream());
+             Scanner scanner = new Scanner(System.in)) {
+
+
+            while (scanner.hasNextLine()) {
+                String mеssage = scanner.nextLine();
+                request.writeUTF(mеssage);
+                System.out.println("Response from the server: " + answer.readUTF());
+            }
+        } catch (IOException e) {
+            System.out.println("Server is unreachable");
+        }
+    }
 }
