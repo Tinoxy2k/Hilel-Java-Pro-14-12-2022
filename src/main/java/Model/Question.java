add2c81d682ebd8af38494794fd9f9d6e92608d7
Tinoxy <Tinoxy2k@gmail.com>
2023-03-28 16:17:36 +0200

    Add Question class

---
+++ src/main/java/Model/Question.java
@@ -0,0 +1,18 @@
+package Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Question {
+    private int id;
+    private String question;
+
+    private String topic;
+
+}
